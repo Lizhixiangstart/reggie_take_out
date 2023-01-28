49521661157bb617c34fc953ec7bd355f10cdda4
Lzx <1181776037@qq.com>
2023-01-28 21:59:57 +0800

    Redis环境

--- src/main/java/com/itheima/reggie/controller/RedircetController.java
+++ src/main/java/com/itheima/reggie/controller/RedircetController.java
@@ -1,5 +1,6 @@
 package com.itheima.reggie.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +18,7 @@ import java.io.IOException;
  */
 @RestController
 public class RedircetController {
-    @RequestMapping
+    @GetMapping("/")
     public void redirectToMainPage(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
         response.sendRedirect("/backend/page/login/login.html");
     }
